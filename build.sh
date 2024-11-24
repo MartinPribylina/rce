950246944307f4974bc18a88311b3550ff4d0021
MartinPribylina <79808540+MartinPribylina@users.noreply.github.com>
2024-11-24 13:30:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-pip install -r requirements.txt
+
+# Update the package list
+sudo apt update
+
+# Install Python 3 and pip
+sudo apt install -y python3 python3-pip python3-venv qtwayland5
+
+#pip install -r requirements.txt
+python3 -m venv my-venv
+
+my-venv/bin/pip install -r requirements.txt
